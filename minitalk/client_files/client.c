6db7302ac6b312aa068c5c195690599face5bca6
Tham Dexmund <tdexmund@u91z04s03.42kl.edu.my>
2024-12-18 17:35:05 +0800

    killme minitalk

--- minitalk/client_files/client.c
+++ minitalk/client_files/client.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdexmund <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 15:11:56 by tdexmund          #+#    #+#             */
+/*   Updated: 2024/12/18 15:11:59 by tdexmund         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/all.h"
 
-int binconv(int num, char *bin, int count)
+int	binconv(int num, char *bin, int count)
 {
 	bin[count] = '0' + (num % 2);
 	if (num > 1)
@@ -8,46 +20,60 @@ int binconv(int num, char *bin, int count)
 	return (count);
 }
 
-int main(int argcount, char **args)
+void sendinitial(int digits, int serverpid)
 {
-	int index;
-	int charcode;
-	int digits;
 	int temp;
-	int serverPID;
-	// int clientPID;
-	char charbin[22];
 
-	// clientPID = getpid();
-	serverPID = ft_atoi(args[1]);
+	temp = 7 - digits;
+	while (temp-- >= 0)
+	{
+		
+		ft_printf("0");
+		kill(serverpid, SIGUSR2);
+		usleep(1000);
+	}
+}
+
+void sendchar(int digits, char *charbin, int serverpid)
+{
+	while (--digits >= 0)
+	{
+		ft_printf("%c", charbin[digits]);
+		if (charbin[digits] == '1')
+			kill(serverpid, SIGUSR1);
+		else
+			kill(serverpid, SIGUSR2);
+		usleep(1000);
+	}
+}
+
+int	main(int argcount, char **args)
+{
+	int		index;
+	int		charcode;
+	int		digits;
+	int		serverpid;
+	char	charbin[22];
+
+	serverpid = ft_atoi(args[1]);
 	if (argcount != 3)
 		return (0);
-	if (serverPID < 0)
+	if (serverpid < 0)
 		return (0);
 	index = -1;
 	while (args[2][++index])
 	{
 		charcode = args[2][index];
-		ft_memset(charbin, 0, sizeof(charbin));
+		ft_memset(charbin, 0, sizeof(char) * 22);
 		digits = binconv(charcode, charbin, 0);
 		digits++;
-		ft_printf("(%c, %d, %s)\n", charcode, digits, charbin);
 		if (digits <= 7)
 		{
-			temp = 7 - digits;
-			while (temp-- >= 0)
-			{
-				kill(serverPID, SIGUSR2);
-				usleep(400);
-			}
-			while (--digits >= 0)
-			{
-				if (charbin[digits] == '1')
-					kill(serverPID, SIGUSR1);
-				else
-					kill(serverPID, SIGUSR2);
-				usleep(400);
-			}
+			sendinitial(digits, serverpid);
+			usleep(1000);
+			sendchar(digits, (char *)charbin, serverpid);
+			sleep(1);
 		}
+		ft_printf("(%c, %d, %s)\n", charcode, digits, charbin);
 	}
 }
--- minitalk/server_files/server.c
+++ minitalk/server_files/server.c
@@ -1,51 +1,35 @@
 #include "../includes/all.h"
 int signalsrecieved;
 
-void processword(unsigned int unicode)
+void processword(unsigned char unicode)
 {
 	unsigned int		temp;
 	unsigned int		powed;
 
 	temp = 0;
 	powed = 1;
-	if (unicode < 10000000)
-	{
-		while (unicode)
-		{
-			temp += (unicode % 10) * powed;
-			if (powed == 1)
-				powed++;
-			else
-				powed *= 2;
-			unicode /= 10;
-		}
-		ft_printf("%c", temp);
-	}
+	if (unicode >> 7 == 0)
+		ft_printf("%c", unicode);
 	else
 		ft_printf("(?)\n");
 }
 
 void signalhandlers(int sigcode)
 {
-	static unsigned int	unicode = 0;
+	static unsigned char	unicode = 0;
 
-	if (sigcode == -1)
-	{
-		unicode = 0;
-		signalsrecieved = 0;
-		return ;
-	}
 	if (sigcode == SIGUSR1)
-		unicode = unicode * 10 + 1;
+		unicode = (unicode << 1) | 1;
 	else if (sigcode == SIGUSR2)
-		unicode *= 10;
+		unicode = (unicode << 1);
 	signalsrecieved++;
 	if (signalsrecieved == 8)
 	{
-		processword(unicode);
 		signalsrecieved = 0;
+		processword(unicode);
 		unicode = 0;
 	}
+	
 }
 
 int main()
@@ -58,7 +42,6 @@ int main()
 	ft_printf("Server's process ID: %u\n", serverpid);
 	handle.sa_handler = signalhandlers;
 	sigemptyset(&handle.sa_mask);
-	
 	sigaction(SIGUSR1, &handle, NULL);
 	sigaction(SIGUSR2, &handle, NULL);
 	while (1)
