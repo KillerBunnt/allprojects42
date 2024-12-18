8c1c68057331a8d4196a461ae30bbd5cc0e62c7c
KillerBunnt <dexmund111@Dexmundlaptop>
2024-12-18 15:05:37 +0800

    verify norminette minitalk

--- minitalk/client_files/client.c
+++ minitalk/client_files/client.c
@@ -25,48 +25,23 @@ int main(int argcount, char **args)
 	if (serverPID < 0)
 		return (0);
 	index = -1;
-	// ft_memset(charbin, 0, sizeof(charbin));
-	// digits = binconv(charcode, charbin, 0);
-	// temp = digits % 8;
-	// while (++temp <= 8)
-	// {
-	// 	kill(serverPID, SIGUSR2);
-	// 	usleep(200);
-	// }
-	// while (--digits >= 0)
-	// {
-	// 	// ft_printf("(%d, %c)", digits, charbin[digits]);
-	// 	if (charbin[digits] == '1')
-	// 		kill(serverPID, SIGUSR1);
-	// 	else
-	// 		kill(serverPID, SIGUSR2);
-	// 	usleep(200);
-	// }
-	// temp = 0;
-	// while (++temp <= 8)
-	// {
-	// 	kill(serverPID, SIGUSR1);
-	// 	usleep(200)
-	// }
 	while (args[2][++index])
 	{
 		charcode = args[2][index];
 		ft_memset(charbin, 0, sizeof(charbin));
 		digits = binconv(charcode, charbin, 0);
 		digits++;
+		ft_printf("(%c, %d, %s)\n", charcode, digits, charbin);
 		if (digits <= 7)
 		{
-			kill(serverPID, SIGUSR2);
-			usleep(400);
 			temp = 7 - digits;
-			while (--temp >= 0)
+			while (temp-- >= 0)
 			{
 				kill(serverPID, SIGUSR2);
 				usleep(400);
 			}
 			while (--digits >= 0)
 			{
-				// ft_printf("(%d, %c)", digits, charbin[digits]);
 				if (charbin[digits] == '1')
 					kill(serverPID, SIGUSR1);
 				else
@@ -74,12 +49,5 @@ int main(int argcount, char **args)
 				usleep(400);
 			}
 		}
-		// else if (digits <= 11)
-
-		// else if (digits <= 16)
-
-		// else if (digits <= 21)
-
-		ft_printf("(%c, %d, %s)\n", charcode, digits, charbin);
 	}
 }
--- minitalk/server_files/server.c
+++ minitalk/server_files/server.c
@@ -1,20 +1,34 @@
 #include "../includes/all.h"
-//idea to do (binary code), wait for first signal, sleep for 200microsec, if
 int signalsrecieved;
 
-// void signal1handler(int sigcode);
-// void signal2handler(int sigcode);
+void processword(unsigned int unicode)
+{
+	unsigned int		temp;
+	unsigned int		powed;
+
+	temp = 0;
+	powed = 1;
+	if (unicode < 10000000)
+	{
+		while (unicode)
+		{
+			temp += (unicode % 10) * powed;
+			if (powed == 1)
+				powed++;
+			else
+				powed *= 2;
+			unicode /= 10;
+		}
+		ft_printf("%c", temp);
+	}
+	else
+		ft_printf("(?)\n");
+}
 
 void signalhandlers(int sigcode)
 {
 	static unsigned int	unicode = 0;
-	// char				codestr[5];
-	unsigned int		temp;
-	unsigned int		powed;
-	// if (temp < 0x0080) //one byte
-	// if (temp < 0x0800) //2 byte
-	// if (temp < 0x10000) //3 byte
-	// if (temp <= 0x10FFFF) //4 byte
+
 	if (sigcode == -1)
 	{
 		unicode = 0;
@@ -22,56 +36,17 @@ void signalhandlers(int sigcode)
 		return ;
 	}
 	if (sigcode == SIGUSR1)
-	{
 		unicode = unicode * 10 + 1;
-		// ft_printf("1");
-	}
 	else if (sigcode == SIGUSR2)
-	{
 		unicode *= 10;
-		// ft_printf("0");
-	}
 	signalsrecieved++;
-	// ft_printf("|%d|", signalsrecieved);
 	if (signalsrecieved == 8)
 	{
-		// ft_printf("%d\n", unicode);
-		if (sigcode < 10000000)
-		{
-			temp = 0;
-			powed = 1;
-			while (unicode)
-			{
-				temp += (unicode % 10) * powed;
-				if (powed == 1)
-					powed++;
-				else
-					powed *= 2;
-				unicode /= 10;
-			}
-			ft_printf("%c", temp);
-		}
-		else
-			ft_printf("NEXTCHAR\n");
+		processword(unicode);
 		signalsrecieved = 0;
 		unicode = 0;
-	}	
+	}
 }
-
-// void signal1handler(int sigcode)
-// {
-// 	ft_printf("RECIEVEDSIG1\n");
-// 	if (signalsrecieved == 8)
-// 	{
-// 		if ()
-// 		ft_printf("NEXTCHAR\n");
-// 	}
-// }
-
-// void signal2handler(int sigcode)
-// {
-
-// }
 
 int main()
 {
@@ -87,11 +62,5 @@ int main()
 	sigaction(SIGUSR1, &handle, NULL);
 	sigaction(SIGUSR2, &handle, NULL);
 	while (1)
-	{
 		pause();
-		// usleep(300);
-		// if (signalsrecieved < 8)
-		// 	signalhandlers(-1);
-	}
-		
 }
